dfe01abad475e522d3a9ed84b0b79adce7665ae0
AJMIK <74661392+AJMIK@users.noreply.github.com>
2021-06-26 20:37:36 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1 className="note">JAVASCRIPT&REACT.JS</h1>
+      <p className="note">
+        Basic Web Dev ReactJS, 7 Days Free Bootcamp on Basic Web Dev in
+        collaboration with Google Developer Groups, Microsoft Learn Student
+        Ambassador Program, and AWS Students Club.
+      </p>
+    </div>
+  );
+}
+export default Info;
